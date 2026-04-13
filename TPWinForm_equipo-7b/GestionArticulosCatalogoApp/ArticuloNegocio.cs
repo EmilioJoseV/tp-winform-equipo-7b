cb874d7dc37e845b7136f96a640ba460d58611f7
AlmadaMauricio <110863446+AlmadaMauricio@users.noreply.github.com>
2026-04-13 20:49:46 -0300

    Se eliminó la clase de Ejemplo y se crearon las clases Articulo y ArticuloNegocio en donde se incluyó la conexion mediante ado.net

---
+++ TPWinForm_equipo-7b/GestionArticulosCatalogoApp/ArticuloNegocio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GestionArticulosCatalogoApp
+{
+    internal class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            //conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+            //comando.CommandType = System.Data.CommandType.Text;
+            //comando.CommandText = "S";
+            //comando.Connection = conexion;
+
+            try
+            {
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex; 
+            }
+        }
+    }
+}
