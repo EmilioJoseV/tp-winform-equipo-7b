658727f99151683f6c97de987f450dfd325273c9
Cyntia-000 <cyntianoemirodriguez>
2026-04-19 12:18:09 -0300

    cambios

--- TPWinForm_equipo-7b/Negocio/ImagenNegocio.cs
+++ TPWinForm_equipo-7b/Negocio/ImagenNegocio.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Negocio
 {
     public class ImagenNegocio
     {
+        public List<Imagen> Listar()
+        {
+            List <Imagen> lista = new List<Imagen>();    
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                return lista;
+            }
+            catch (Exception ex)
+            {
 
+                throw ex;
+            }
+
+
+        }
     }
 }
--- TPWinForm_equipo-7b/Negocio/MarcaNegocio.cs
+++ TPWinForm_equipo-7b/Negocio/MarcaNegocio.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Negocio
 {
     public class MarcaNegocio
